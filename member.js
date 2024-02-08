d068699c0af6d09d528c07daea22d165cd3b48b4
Cesar Pineda <69724879+htmtmx@users.noreply.github.com>
2024-02-08 18:41:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,33 @@
+function skillsMember(){
+    var data = [
+      {
+        name: "Budi",
+        age: 23,
+        skills: {
+          programming: "javascript",
+          framework: "react",
+          database: "MongoDB",
+        },
+      },
+      {
+        name: "Susi",
+        age: 25,
+        skills: {
+          programming: "java",
+          framework: "Spring",
+          database: "MySQL",
+        },
+      },
+      {
+        name: "Lala",
+        age: 21,
+        skills: {
+          programming: "c#",
+          framework: "ASP.Net",
+          database: "SQL Server",
+        },
+      },
+    ];
+    return data;
+  }
+}
